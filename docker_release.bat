478808d8ac7f81d829f03d52a6300608b80f975b
Lesko <matevzlesko@gmail.com>
2025-04-04 12:43:36 +0200

    Bump version 0.0.2 -> 0.0.3.

--- docker_release.bat
+++ docker_release.bat
@@ -1,10 +1,10 @@
 docker image ls
 
-docker build -t leskoiam/openairelay:0.0.2 .
+docker build -t leskoiam/openairelay:0.0.3 .
 docker image ls
 
-docker tag leskoiam/openairelay:0.0.2 leskoiam/openairelay:latest
+docker tag leskoiam/openairelay:0.0.3 leskoiam/openairelay:latest
 docker image ls
 
-docker push leskoiam/openairelay:0.0.2
+docker push leskoiam/openairelay:0.0.3
 docker push leskoiam/openairelay:latest
